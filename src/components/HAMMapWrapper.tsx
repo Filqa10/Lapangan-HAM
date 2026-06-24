de9218768b17e7cf71c51ea87f25911895cd0281
royanrosyad85 <royan.rosyad21@mhs.uinjkt.ac.id>
2026-06-25 01:00:04 +0700

    fix: update Stadion HAM coordinates to 6°22'33.0"S 106°47'18.8"E

--- src/components/HAMMapWrapper.tsx
+++ src/components/HAMMapWrapper.tsx
@@ -4,9 +4,9 @@ import { Map, MapControls, MapMarker, MarkerContent } from './ui/map';
 
 export default function HAMMapWrapper() {
   return (
-    <Map center={[112.4414, -7.2185]} zoom={15} className="h-full w-full text-[#0c0a08]">
+    <Map center={[106.788556, -6.375833]} zoom={15} className="h-full w-full text-[#0c0a08]">
       <MapControls />
-      <MapMarker longitude={112.4414} latitude={-7.2185}>
+      <MapMarker longitude={106.788556} latitude={-6.375833}>
         <MarkerContent>
           <div className="flex h-9 w-9 items-center justify-center rounded-full border border-white bg-[#0c0a08] text-[16px] shadow-lg">
             🏟️
